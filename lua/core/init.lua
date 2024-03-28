4f4977a4ec5b07c8d0e6f0c02ecb60e0e764629a
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2024-03-28 21:01:48 +0100

    feat: automatically install packer

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,46 +2,6 @@ require("core.set")
 require("core.remap")
 require("core.packer")
 
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
-local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
-
-local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
-
-function R(name)
-    require("plenary.reload").reload_module(name)
-end
-
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
-})
-
-autocmd({"BufWritePre"}, {
-    group = ThePrimeagenGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
-})
-
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,9 +1,18 @@
-vim.cmd.packadd('packer.nvim')
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use({
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
     requires = { {'nvim-lua/plenary.nvim'} }
   })
   use({ "catppuccin/nvim", as = "catppuccin" })
@@ -40,6 +49,8 @@ return require('packer').startup(function(use)
         {'rafamadriz/friendly-snippets'},
     }
   }
-
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
